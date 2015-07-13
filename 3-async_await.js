84770c8df605194d5cd73272de541ba03e73796a
Joe Pettersson <joe8bit@gmail.com>
2015-07-13 14:39:48 -0400

    make 3-async more consistent

--- 3-async_await.js
+++ 3-async_await.js
@@ -1,9 +1,7 @@
 'use strict';
 
 async function loadUser() {
-  let user = await window.fetch('user.json', { method: 'GET' });
-  user.greeting = `Hi ${user.firstName}, Welcome to Foobar.com!`;
-  return user;
+  return await window.fetch('user.json', { method: 'GET' });
 }
 
 async function loadStories(userId) {
@@ -13,8 +11,8 @@ async function loadStories(userId) {
 async function init() {
   let data = {};
   data.user = await loadUser();
+  data.user.greeting = `Hi ${user.firstName}, Welcome to Foobar.com!`;
   data.stories = await loadStories(data.user.id);
-
   return data;
 };
 
