f77a686800dca4ca1e7e6d2babb66c8302f20b31
Joe Pettersson <joe8bit@gmail.com>
2015-07-13 12:14:07 -0400

    remove comments from async-await example

--- 3-async_await.js
+++ 3-async_await.js
@@ -1,7 +1,5 @@
-// Create an asyncrhonous function using the ES7 syntax
-// If fetch rejects, loadUser will throw an exception with the arugments passed to reject
 async function loadUser() {
-  let user = await window.fetch('user.json', { method: 'GET' }); // fetch returns a promise
+  let user = await window.fetch('user.json', { method: 'GET' });
   user.greeting = `Hi ${user.firstName}, Welcome to Foobar.com!`;
   return user;
 }
@@ -10,7 +8,6 @@ async function loadStories(userId) {
   return await window.fetch(userId + '/stories.json', { method: 'GET' });
 }
 
-// Export a function that is also async
 module.exports = async function() {
   let data = {};
   data.user = await loadUser();
