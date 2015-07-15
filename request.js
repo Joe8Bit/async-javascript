090c420d2d6684c10d8790f5f1fac2654184a634
Joe Pettersson <joe8bit@gmail.com>
2015-07-15 14:56:32 +0530

    fix whitespace

--- request.js
+++ request.js
@@ -5,36 +5,36 @@
 
 class Request {
 
-	map = new Map()
-				.set('user.json', {
-					id: 2,
-					firstName: 'Steve'
-				})
-				.set('2/stories.json', [{
-					foo: 'bar'
-				}])
+  map = new Map()
+        .set('user.json', {
+          id: 2,
+          firstName: 'Steve'
+        })
+        .set('2/stories.json', [{
+          foo: 'bar'
+        }])
 
-	constructor() {}
+  constructor() {}
 
-	callMeMaybe(url, next, isPromise = false) {
-		setTimeout(() => {
-			if (isPromise) {
-				next(this.map.get(url));
-			} else {
-				next(null, this.map.get(url));
-			}
-		}, 100);
-	}
+  callMeMaybe(url, next, isPromise = false) {
+    setTimeout(() => {
+      if (isPromise) {
+        next(this.map.get(url));
+      } else {
+        next(null, this.map.get(url));
+      }
+    }, 100);
+  }
 
-	get(url, next) {
-		if (next) {
-			this.callMeMaybe(url, next);
-		} else {
-			return new Promise((resolve, reject) => {
-				this.callMeMaybe(url, resolve, true)
-			});
-		}
-	}
+  get(url, next) {
+    if (next) {
+      this.callMeMaybe(url, next);
+    } else {
+      return new Promise((resolve, reject) => {
+        this.callMeMaybe(url, resolve, true)
+      });
+    }
+  }
 
 }
 
